ec6a48ad44c66ab0f7c37508bfe13005ce0c45b0
leo-liuzy <zeyuliu2@cs.washington.edu>
2020-02-18 12:56:13 -0800

    update files with clarification from paper author

---
+++ test_modified_lamol.py
@@ -0,0 +1,177 @@
+from pytorch_transformers import AdamW, WarmupLinearSchedule, WarmupConstantSchedule
+from torch import optim
+from torch.utils.data import DataLoader
+import argparse
+import copy
+import logging
+import numpy as np
+import os
+import pickle
+import torch
+logger = logging.getLogger(__name__)
+logging.getLogger("pytorch_transformers").setLevel(logging.WARNING)
+
+from settings import parse_test_args, model_classes, init_logging
+from utils import TextClassificationDataset, dynamic_collate_fn, prepare_inputs, DynamicBatchSampler
+from ipdb import set_trace as bp
+
+
+def local_adapt(input_ids, label, tmp_model, q_input_ids, q_masks, q_labels, args, org_params):
+
+    q_input_ids = q_input_ids.cuda().detach()
+    q_masks = q_masks.cuda().detach()
+    q_labels = q_labels.cuda().detach()
+
+    # optimizer = optim.SGD(tmp_model.parameters(), lr=args.adapt_lr, momentum=0.9)
+    optimizer = torch.optim.Adam(tmp_model.parameters(), lr=args.adapt_lr, eps=args.adam_epsilon)
+
+    def predict(model, input_ids, label):
+        with torch.no_grad():
+            tmp_model.eval()
+            output = model(input_ids=input_ids, labels=label)[:2]
+            loss = output[0].item()
+            logits = output[1].detach().cpu().numpy()
+            torch.cuda.empty_cache()
+        return loss, logits
+    accs = []
+    losses = []
+    for step in range(args.adapt_steps + 1):
+        # make predictions
+        loss, logits = predict(tmp_model, input_ids, label)
+        losses.append(loss)
+        accs.append(int(np.argmax(logits, axis=1) == label.detach().cpu().numpy()))
+        if step == args.adapt_steps:
+            break
+        # make 1 local step
+        tmp_model.train()
+        params = torch.cat([torch.reshape(param, [-1]) for param in tmp_model.parameters()], 0)
+        output = tmp_model(input_ids=q_input_ids, attention_mask=q_masks, labels=q_labels)[:2]
+        loss = output[0] + args.adapt_lambda * torch.sum((org_params - params) ** 2)
+        tmp_model.zero_grad()
+        loss.backward()
+        # torch.nn.utils.clip_grad_norm_(tmp_model.parameters(), args.max_grad_norm)
+        optimizer.step()
+        
+    assert len(accs) == len(losses) == args.adapt_steps + 1
+    return accs, losses
+
+def plot_acc_and_loss(accs, losses, file_name):
+    assert len(accs) == len(losses)
+    mean_losses = np.mean(losses, axis=0)
+    mean_acces = np.mean(accs, axis=0)
+    local_steps = np.arange(0, len(mean_acces))
+    import matplotlib.pyplot as plt
+    from matplotlib.ticker import MaxNLocator
+
+    fig, (ax1, ax2) = plt.subplots(2)
+    ax1.plot(local_steps, mean_losses, marker='o')
+    ax2.plot(local_steps, mean_acces, marker='o')
+    ax1.set_ylabel("loss")
+    ax2.set_xlabel("no. of adapt_steps")
+    ax2.set_ylabel("acc")
+    ax1.get_shared_x_axes().join(ax1, ax2)
+    ax1.xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax2.xaxis.set_major_locator(MaxNLocator(integer=True))
+    fig.suptitle(f'ntest{len(accs)}')
+    plt.savefig(f"{file_name}_ntest{len(accs)}.png")
+
+
+
+def test_task(task_id, args, model, test_dataset):
+    # bp() 
+    if args.fp16_test:
+        model = model.half()
+
+    test_size = args.n_test
+    cur_loss, cur_acc = 0, 0
+    if args.adapt_steps >= 1:
+        cur_accs = []
+        cur_losses = []
+        with torch.no_grad():
+            org_params = torch.cat([torch.reshape(param, [-1]) for param in model.parameters()], 0)
+
+        q_input_ids = pickle.load(open(os.path.join(args.output_dir, 'q_input_ids-{}'.format(task_id)), 'rb'))
+        q_masks = pickle.load(open(os.path.join(args.output_dir, 'q_masks-{}'.format(task_id)), 'rb'))
+        q_labels = pickle.load(open(os.path.join(args.output_dir, 'q_labels-{}'.format(task_id)), 'rb'))
+
+        for i in range(test_size):
+            labels, input_ids = test_dataset[i]
+            labels = torch.tensor(np.expand_dims(labels, 0), dtype=torch.long).cuda()
+            input_ids = torch.tensor(np.expand_dims(input_ids, 0), dtype=torch.long).cuda()
+            accs, losses = local_adapt(input_ids, labels, copy.deepcopy(model), q_input_ids[i], q_masks[i], q_labels[i], args, org_params)
+            if len(cur_accs) == len(cur_losses) == 0:
+                cur_losses = np.array([losses])
+                cur_accs = np.array([accs])
+            else:
+                cur_losses = np.concatenate([cur_losses, [losses]])
+                cur_accs = np.concatenate([cur_accs, [accs]])
+            
+            if (i+1) % args.logging_steps == 0:
+                logging.info("Local adapted {}/{} examples, test loss: {:.3f} , test acc: {:.3f}".format(
+                    i+1, test_size, cur_losses.mean(axis=0)[-1],
+                    cur_accs.mean(axis=0)[-1]))
+        pickle.dump({"accuracy": cur_accs, "loss": cur_losses},
+                open(f"{args.output_dir}/lr{args.adapt_lr}_metrics_against_adapt_step_{task_id}_ntest{args.n_test}", "wb"))
+        plot_acc_and_loss(accs=cur_accs, losses=cur_losses,
+                file_name=f"{args.output_dir}/lr{args.adapt_lr}_metrics_against_adapt_step_plot_{task_id}")
+        logger.info("test loss: {:.3f} , test acc: {:.3f}".format(
+            cur_losses.mean(axis=0)[-1], cur_accs.mean(axis=0)[-1]))
+        return cur_accs.mean(axis=0), cur_losses.mean(axis=0)
+
+    else:
+        test_dataloader = DataLoader(test_dataset, num_workers=args.n_workers, collate_fn=dynamic_collate_fn,
+                                     batch_sampler=DynamicBatchSampler(test_dataset, args.batch_size * 4))
+        tot_n_inputs = 0
+        for step, batch in enumerate(test_dataloader):
+            n_inputs, input_ids, masks, labels = prepare_inputs(batch)
+            tot_n_inputs += n_inputs
+            with torch.no_grad():
+                model.eval()
+                outputs = model(input_ids=input_ids, attention_mask=masks, labels=labels)
+                loss = outputs[0].item()
+                logits = outputs[1].detach().cpu().numpy()
+            cur_loss, cur_acc = update_metrics(loss*n_inputs, logits, cur_loss, cur_acc)
+            if (step+1) % args.logging_steps == 0:
+                logging.info("Tested {}/{} examples , test loss: {:.3f} , test acc: {:.3f}".format(
+                    tot_n_inputs, len(test_dataset), cur_loss/tot_n_inputs, cur_acc/tot_n_inputs))
+        assert tot_n_inputs == len(test_dataset)
+
+
+        logger.info("test loss: {:.3f} , test acc: {:.3f}".format(
+            cur_loss / test_size, cur_acc / test_size))
+        return cur_acc / test_size
+
+
+def main():
+    args = parse_test_args()
+    train_args = pickle.load(open(os.path.join(args.output_dir, 'train_args'), 'rb'))
+    # assert train_args.output_dir == args.output_dir
+    train_args.__dict__.update(args.__dict__)
+    args = train_args
+    init_logging(os.path.join(args.output_dir, args.test_log_filename))
+    logger.info("args: " + str(args))
+
+    config_class, model_class, args.tokenizer_class = model_classes[args.model_type]
+    model_config = config_class.from_pretrained(args.model_name, num_labels=args.n_labels, hidden_dropout_prob=0, attention_probs_dropout_prob=0)
+    save_model_path = os.path.join(args.output_dir, 'checkpoint-{}'.format(len(args.tasks)-1))
+    model = model_class.from_pretrained(save_model_path, config=model_config).cuda()
+
+    avg_accs = []
+    avg_losses = []
+    for task_id, task in enumerate(args.tasks):
+        logger.info("Start testing {}...".format(task))
+        test_dataset = pickle.load(open(os.path.join(args.output_dir, 'test_dataset-{}'.format(task_id)), 'rb'))
+        task_acc, task_loss = test_task(task_id, args, model, test_dataset)
+        avg_accs.append(task_acc)
+        avg_losses.append(task_loss)
+    avg_accs = np.array(avg_accs)
+    avg_losses = np.array(avg_losses)
+
+    plot_acc_and_loss(accs=avg_accs, losses=avg_losses,
+            file_name=f"{args.output_dir}/lr{args.adapt_lr}_metrics_against_adapt_step_plot_avg")
+    logger.info("Average acc: {:.3f}".format(avg_accs.mean(axis=0)[-1]))
+    logger.info("Average loss: {:.3f}".format(avg_losses.mean(axis=0)[-1]))
+
+
+if __name__ == "__main__":
+    main()
---
+++ train_one_optimizer.py
@@ -0,0 +1,143 @@
+from pytorch_transformers import AdamW, WarmupLinearSchedule
+from torch.utils.data import DataLoader, SequentialSampler
+import logging
+import numpy as np
+import os
+import pickle
+import torch
+from torch import optim
+from ipdb import set_trace as bp
+logger = logging.getLogger(__name__)
+logging.getLogger("pytorch_transformers").setLevel(logging.WARNING)
+
+from memory import Memory
+from settings import parse_train_args, model_classes, init_logging
+from utils import TextClassificationDataset, DynamicBatchSampler
+from utils import dynamic_collate_fn, prepare_inputs
+
+
+def query_neighbors(task_id, args, memory, test_dataset):
+    test_dataloader = DataLoader(test_dataset, num_workers=args.n_workers, collate_fn=dynamic_collate_fn, 
+            batch_sampler=DynamicBatchSampler(test_dataset, args.batch_size, mode="seq"))
+
+
+    q_input_ids, q_masks, q_labels = [], [], []
+    for step, batch in enumerate(test_dataloader):
+        n_inputs, input_ids, masks, labels = prepare_inputs(batch)
+        with torch.no_grad():
+            cur_q_input_ids, cur_q_masks, cur_q_labels = memory.query(input_ids, masks)
+        q_input_ids.extend(cur_q_input_ids)
+        q_masks.extend(cur_q_masks)
+        q_labels.extend(cur_q_labels)
+        if (step+1) % args.logging_steps == 0:
+            logging.info("Queried {} examples".format(len(q_masks)))
+    pickle.dump(q_input_ids, open(os.path.join(args.output_dir, 'q_input_ids-{}'.format(task_id)), 'wb'))
+    pickle.dump(q_masks, open(os.path.join(args.output_dir, 'q_masks-{}'.format(task_id)), 'wb'))
+    pickle.dump(q_labels, open(os.path.join(args.output_dir, 'q_labels-{}'.format(task_id)), 'wb'))
+
+
+def train_task(args, model, memory, optimizer, train_dataset, valid_dataset):
+
+    # train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, num_workers=args.n_workers,
+    #                               shuffle=not args.reproduce, collate_fn=dynamic_collate_fn)
+    train_dataloader = DataLoader(train_dataset, num_workers=args.n_workers, collate_fn=dynamic_collate_fn, 
+            batch_sampler=DynamicBatchSampler(train_dataset, args.batch_size, mode=args.sampler_choice))
+    # if valid_dataset:
+    #     valid_dataloader = DataLoader(valid_dataset, batch_size=args.batch_size * 6,
+    #                                   num_workers=args.n_workers, collate_fn=dynamic_collate_fn)
+    # model.zero_grad()
+    tot_epoch_loss, tot_n_inputs = 0, 0
+
+    def update_parameters(loss):
+        model.zero_grad()
+        loss.backward()
+        # torch.nn.utils.clip_grad_norm_(model.parameters(), args.max_grad_norm)
+        # scheduler.step()
+        optimizer.step()
+        # model.zero_grad()
+    
+    from ipdb import set_trace as bp
+    for step, batch in enumerate(train_dataloader):
+        model.train()
+        n_inputs, input_ids, masks, labels = prepare_inputs(batch)
+        memory.add(input_ids, masks, labels)
+        loss = model(input_ids=input_ids, attention_mask=masks, labels=labels)[0]
+        update_parameters(loss)
+        tot_n_inputs += n_inputs
+        tot_epoch_loss += loss.item() * n_inputs
+
+        if (step+1) % args.logging_steps == 0:
+            non_decay_lr = [group["lr"] * (-1e-5 <= group["weight_decay"] - 0.0 <= 1e-5) for group in optimizer.param_groups][0]
+            decay_lr = [group["lr"] * (group["weight_decay"] - 0.0 > 1e-5) for group in optimizer.param_groups][0]
+            # bp()
+            logger.info("progress: {:.2f} , step: {} , decay_lr: {:.2E} , non_decay_lr: {:.2E} , avg batch size: {:.1f} , avg loss: {:.3f}".format(
+                tot_n_inputs/args.n_train, step+1, decay_lr, non_decay_lr, tot_n_inputs//(step+1), tot_epoch_loss/tot_n_inputs))
+
+        if args.replay_interval >= 1 and (step+1) % args.replay_interval == 0:
+            torch.cuda.empty_cache()
+            # del loss, input_ids, masks, labels
+            input_ids, masks, labels = memory.sample(tot_n_inputs // (step + 1))
+            loss = model(input_ids=input_ids, attention_mask=masks, labels=labels)[0]
+            update_parameters(loss)
+
+
+    logger.info("Finsih training, avg loss: {:.3f}".format(tot_epoch_loss/tot_n_inputs))
+    assert tot_n_inputs == len(train_dataset) == args.n_train
+
+
+def main():
+    args = parse_train_args()
+    pickle.dump(args, open(os.path.join(args.output_dir, 'train_args'), 'wb'))
+    init_logging(os.path.join(args.output_dir, 'log_train.txt'))
+    logger.info("args: " + str(args))
+
+    logger.info("Initializing main {} model".format(args.model_name))
+    config_class, model_class, args.tokenizer_class = model_classes[args.model_type]
+    tokenizer = args.tokenizer_class.from_pretrained(args.model_name)
+
+    model_config = config_class.from_pretrained(args.model_name, num_labels=args.n_labels)
+    config_save_path = os.path.join(args.output_dir, 'config')
+    model_config.to_json_file(config_save_path)
+    model = model_class.from_pretrained(args.model_name, config=model_config).cuda()
+    memory = Memory(args)
+    
+    no_decay = ['bias', 'LayerNorm.weight']
+    optimizer_grouped_parameters = [
+        {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)], 'weight_decay': args.weight_decay},
+        {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+    ]
+    optimizer = optim.Adam(optimizer_grouped_parameters, lr=args.learning_rate, eps=args.adam_epsilon)
+    # scheduler = WarmupLinearSchedule(optimizer, warmup_steps=args.warmup_steps, t_total=len(args.tasks) * args.n_train//args.batch_size)
+
+
+    for task_id, task in enumerate(args.tasks):
+        logger.info("Start parsing {} train data...".format(task))
+        train_dataset = TextClassificationDataset(task, "train", args, tokenizer)
+
+        logger.info("Max length: {}...".format(train_dataset.max_len))
+        if args.valid_ratio > 0:
+            logger.info("Start parsing {} valid data...".format(task))
+            valid_dataset = TextClassificationDataset(task, "valid", args, tokenizer)
+        else:
+            valid_dataset = None
+
+        logger.info("Start training {}...".format(task))
+        train_task(args, model, memory, optimizer, train_dataset, valid_dataset)
+        model_save_path = os.path.join(args.output_dir, 'checkpoint-{}'.format(task_id))
+        torch.save(model.state_dict(), model_save_path)
+        pickle.dump(memory, open(os.path.join(args.output_dir, 'memory-{}'.format(task_id)), 'wb'))
+
+
+    del model
+    memory.build_tree()
+
+    for task_id, task in enumerate(args.tasks):
+        logger.info("Start parsing {} test data...".format(task))
+        test_dataset = TextClassificationDataset(task, "test", args, tokenizer)
+        pickle.dump(test_dataset, open(os.path.join(args.output_dir, 'test_dataset-{}'.format(task_id)), 'wb'))
+        logger.info("Start querying {}...".format(task))
+        query_neighbors(task_id, args, memory, test_dataset)
+
+
+if __name__ == "__main__":
+    main()
