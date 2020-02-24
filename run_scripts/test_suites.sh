3311fe8d5ec14608f0e25f6f0631a8b74d3b4a59
leo-liuzy <zeyuliu2@cs.washington.edu>
2020-02-23 21:44:18 -0800

    group running scripts

---
+++ run_scripts/test_suites.sh
@@ -0,0 +1,2 @@
+python test_modified.py --output_dir $1 --adapt_steps 30 --n_test 200 --logging_steps 20 --test_log_filename log_test_adam.txt --adapt_lr 1e-3
+python test_modified.py --output_dir $1 --adapt_steps 30 --n_test 200 --logging_steps 20 --test_log_filename log_test_adam.txt --adapt_lr 5e-3
